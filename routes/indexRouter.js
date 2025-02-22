454ecee7357a27f1261158c1f56f579e854cf7ed
James Zhou <jam9es@gmail.com>
2025-02-21 22:22:13 -0800

    Add demo messages on the index page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,23 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
 // route handler
 // load EJS index template
 indexRouter.get("/", (req, res) => {
-    res.render("index", { message: "EJS is working properly!" });
+    res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = indexRouter;
