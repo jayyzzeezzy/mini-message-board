ab09fa84379c7681d355db57128691625da1d633
James Zhou <jam9es@gmail.com>
2025-02-22 16:26:05 -0800

    Write CSS style to center all contents

--- app.js
+++ app.js
@@ -12,15 +12,20 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// set up static files that is CSS stylesheet
+// using an Express middleware
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+// set up an app level Express middleware
+// to parse form data in a POST request
+app.use(express.urlencoded({ extended: true }));
+
 // set up route
 // EJS template is used in this route
 const {indexRouter} = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
 const messageRouter = require("./routes/messageRouter");
-
-// set up an app level Express middleware
-// to parse form data in a POST request
-app.use(express.urlencoded({ extended: true }));
 
 // define routes and their corresponding middleware
 app.use("/message", messageRouter);
